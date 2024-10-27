27b12e954e4583c32bdb061e9fc3b45990895f9e
abdellah karani <abdellahkarani@gamil.com>
2024-10-27 18:16:06 +0100

    .

--- main.js
+++ main.js
@@ -1,34 +1,98 @@
 // Start Header
-const shop_button = document.querySelector("button.shop");
-const choices_eles = document.querySelector(".choices-eles");
-const cancel_icon = document.querySelector("button.cancel");
-const angle_up = document.querySelector(".cart .range .angle-up");
-const angle_down = document.querySelector(".cart .range .angle-down");
-const number_eles = document.querySelector(".cart .range .number-eles");
+// const angle_up = document.querySelector(".cart .range .angle-up");
+// const angle_down = document.querySelector(".cart .range .angle-down");
+// const cart_price = document.querySelector(".price_ele .price");
 
+
+
+
+
+
+// Start Header
+const choices_eles = document.querySelector(".choices-eles");
+const shop_button = document.querySelector("button.shop");
 shop_button.addEventListener("click", function() {
     choices_eles.classList.toggle("show-choices");
 });
-
+const cancel_icon = document.querySelector("button.cancel");
 cancel_icon.addEventListener("click", function() {
     choices_eles.classList.remove("show-choices");
 });
 
-angle_down.addEventListener("click", function() {
-    if (number_eles.textContent <= 0) {
-        number_eles.textContent =  0;
+
+// Start Our Products Section
+const add_cards = document.querySelectorAll(".badge");
+const carts = document.querySelector(".carts");
+const shop_nbr = document.querySelector(".shop_nbr");
+const totale_price = document.querySelector("span.total-price");
+add_cards.forEach(element => {
+    element.addEventListener("click", function() {
+        const ele_name = element.nextElementSibling.firstElementChild.textContent;
+        const image_ele = element.parentElement.firstElementChild.src;
+        const price_ele = element.nextElementSibling.firstElementChild
+        .nextElementSibling.firstElementChild.textContent;
+        // create cart element
+        const cart_ele = document.createElement("div");
+        cart_ele.innerHTML = `
+        <div class="cart p-2 mb-2 d-flex justify-content-between align-items-center">
+            <div class="image pe-3">
+                <img src="${image_ele}" alt="">
+            </div>
+            <div class="desc text-start w-100">
+                <p class="name mb-0 fw-bold">${ele_name}</p>
+                <p class="price_ele mb-0 fw-bold">$<span class="price">${price_ele}</span></p>
+                <p class="remove mb-0 text-danger cursor-pointer">remove</p>
+            </div>
+            <div class="range">
+                <i class="fa-solid fa-angle-up angle-up cursor-pointer"></i>
+                <div class="number-eles">0</div>
+                <i class="fa-solid fa-angle-down angle-down  cursor-pointer"></i>
+            </div>
+        </div>
+        `;
+        carts.appendChild(cart_ele);
+        shop_nbr.textContent = Number(shop_nbr.textContent) + 1;
+    });
+});
+
+// Remove an Element
+carts.addEventListener("click", function(event) {
+    if (event.target.classList.contains("remove")) {
+        const remove_cart = event.target.parentElement.parentElement;
+        remove_cart.remove();
+        shop_nbr.textContent = Number(shop_nbr.textContent) - 1;
     }
-    else number_eles.textContent = Number(number_eles.textContent) - 1;
-});
-angle_up.addEventListener("click", function() {
-    number_eles.textContent = Number(number_eles.textContent) + 1;
 });
 
+// Decrease || Increase
+carts.addEventListener("click", function(event) {
+    if (event.target.classList.contains("angle-down")) {
+        const angle_down = event.target;
+        angle_down.addEventListener("click", function() {
+            const number_eles = angle_down.parentElement.nextElementSibling.nextElementSibling; 
+            if (number_eles.textContent <= 0) {
+                number_eles.textContent =  0;
+            }
+            else {
+                number_eles.textContent = Number(number_eles.textContent) - 1;
+                // totale_price.textContent = Number(totale_price.textContent) - Number(cart_price.textContent); 
+            }
+        });
+    }
+    if (event.target.classList.contains("angle-up")) {
+        const angle_up = event.target;
+        const number_eles = angle_up.nextElementSibling;
+        console.log(angle_up); 
+        console.log(number_eles); 
+        angle_up.addEventListener("click", function() {
+            number_eles.textContent = Number(number_eles.textContent) + 1;
+            // totale_price.textContent = Number(totale_price.textContent) + Number(cart_price.textContent); 
+        });
+    }
+});
 
 
 
 
-
-
-
+/// custom totale price in remove and also in increase and decrease
 
