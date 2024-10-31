3cba5e4be220cb3b0238ae00b63e96a512578634
AbdellahKarani <abdellahkarani@gmail.com>
2024-10-31 17:29:49 +0100

    custom some instruction

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// Start Header
-// const angle_up = document.querySelector(".cart .range .angle-up");
-// const angle_down = document.querySelector(".cart .range .angle-down");
-// const cart_price = document.querySelector(".price_ele .price");
-
-
-
-
-
 
 // Start Header
 const choices_eles = document.querySelector(".choices-eles");
@@ -14,6 +5,7 @@ const shop_button = document.querySelector("button.shop");
 shop_button.addEventListener("click", function() {
     choices_eles.classList.toggle("show-choices");
 });
+
 const cancel_icon = document.querySelector("button.cancel");
 cancel_icon.addEventListener("click", function() {
     choices_eles.classList.remove("show-choices");
@@ -24,40 +16,47 @@ cancel_icon.addEventListener("click", function() {
 const add_cards = document.querySelectorAll(".badge");
 const carts = document.querySelector(".carts");
 const shop_nbr = document.querySelector(".shop_nbr");
-const totale_price = document.querySelector("span.total-price");
+const totale_cart_price = document.querySelector("span.total-price");
 
-add_cards.forEach(element => {
-    element.addEventListener("click", function() {
-        const ele_name = element.nextElementSibling.firstElementChild.textContent;
-        const image_ele = element.parentElement.firstElementChild.src;
-        const price_ele = element.nextElementSibling.firstElementChild
-        .nextElementSibling.firstElementChild.textContent;
-        // create cart element
-        const cart_ele = document.createElement("div");
-        cart_ele.innerHTML = `
-        <div class="cart p-2 mb-2 d-flex justify-content-between align-items-center">
-            <div class="image pe-3">
-                <img src="${image_ele}" alt="">
+add_cards.forEach(cart => {
+    cart.addEventListener("click", function() {
+        const badge_value = cart.firstElementChild.nextElementSibling.textContent;
+        if (badge_value != "Cart Added") {
+            // Get the price & image & name of cart
+            const cart_name = cart.nextElementSibling.firstElementChild.textContent;
+            const cart_image = cart.parentElement.firstElementChild.src;
+            const cart_price = cart.nextElementSibling.firstElementChild
+            .nextElementSibling.firstElementChild.textContent;
+            // create & add cart cart
+            const cart_ele = document.createElement("div");
+            cart_ele.innerHTML = `
+            <div class="cart p-2 mb-2 d-flex justify-content-between align-items-center">
+                <div class="image pe-3">
+                    <img src="${cart_image}" alt="">
+                </div>
+                <div class="desc text-start w-100">
+                    <p class="name mb-0 fw-bold">${cart_name}</p>
+                    <p class="price mb-0 fw-bold">$<span class="price">${cart_price}</span></p>
+                    <p class="remove mb-0 text-danger cursor-pointer">remove</p>
+                </div>
+                <div class="range">
+                    <i class="fa-solid fa-angle-up angle-up cursor-pointer"></i>
+                    <div class="number-eles">0</div>
+                    <i class="fa-solid fa-angle-down angle-down  cursor-pointer"></i>
+                </div>
             </div>
-            <div class="desc text-start w-100">
-                <p class="name mb-0 fw-bold">${ele_name}</p>
-                <p class="price_ele mb-0 fw-bold">$<span class="price">${price_ele}</span></p>
-                <p class="remove mb-0 text-danger cursor-pointer">remove</p>
-            </div>
-            <div class="range">
-                <i class="fa-solid fa-angle-up angle-up cursor-pointer"></i>
-                <div class="number-eles">0</div>
-                <i class="fa-solid fa-angle-down angle-down  cursor-pointer"></i>
-            </div>
-        </div>
-        `;
-        carts.appendChild(cart_ele);
-        // Here
-        shop_nbr.textContent = Number(shop_nbr.textContent) + 1;
-        totale_price.textContent = Number(totale_price.textContent) + Number(price_ele);
+            `;
+            carts.appendChild(cart_ele);
+            // Custom Numver of things that you shop
+            shop_nbr.textContent = Number(shop_nbr.textContent) + 1;
+            totale_cart_price.textContent = Number(totale_cart_price.textContent) + Number(cart_price);
+            // Make Cart Add
+            cart.firstElementChild.nextElementSibling.textContent = "Cart Added";
+        }
     });
 });
 
+// Here
 // Remove an Element
 carts.addEventListener("click", function(event) {
     if (event.target.classList.contains("remove")) {
@@ -66,8 +65,8 @@ carts.addEventListener("click", function(event) {
         const price_ele = event.target.parentElement.firstElementChild.nextElementSibling.
         firstElementChild.textContent;
 
-        totale_price.textContent = Number(totale_price.textContent) - Number(price_ele);
-        if (totale_price.textContent < 0)  totale_price.textContent = 0;
+        totale_cart_price.textContent = Number(totale_cart_price.textContent) - Number(price_ele);
+        if (totale_cart_price.textContent < 0)  totale_cart_price.textContent = 0;
 
         remove_cart.remove();
         shop_nbr.textContent = Number(shop_nbr.textContent) - 1;
@@ -87,7 +86,7 @@ carts.addEventListener("click", function(event) {
         }
         else {
             number_eles.textContent = Number(number_eles.textContent) - 1;
-            totale_price.textContent = Number(totale_price.textContent) - Number(cart_price.textContent); 
+            totale_cart_price.textContent = Number(totale_cart_price.textContent) - Number(cart_price.textContent); 
             shop_nbr.textContent = Number(shop_nbr.textContent) - 1;
         }
     }
@@ -98,7 +97,7 @@ carts.addEventListener("click", function(event) {
         nextElementSibling.firstElementChild.nextElementSibling.firstElementChild;
 
         number_eles.textContent = Number(number_eles.textContent) + 1;
-        totale_price.textContent = Number(totale_price.textContent) + Number(cart_price.textContent); 
+        totale_cart_price.textContent = Number(totale_cart_price.textContent) + Number(cart_price.textContent); 
         shop_nbr.textContent = Number(shop_nbr.textContent) + 1;
     }
 });
